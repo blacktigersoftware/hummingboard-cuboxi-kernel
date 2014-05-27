6fc170bb7bba5917a1515bfa3dc78c091dfbcde3
Rabeeh Khoury <rabeeh@solid-run.com>
2014-05-27 21:21:46 +0300

    Rename sgtl5000_sabrelite* to sgtl5000_hb
    Signed-off-by: Rabeeh Khoury <rabeeh@solid-run.com>

--- arch/arm/mach-mx6/board-mx6q_hb.c
+++ arch/arm/mach-mx6/board-mx6q_hb.c
@@ -357,78 +357,78 @@ static struct imx_esai_platform_data sab_esai_pdata = {
 };
 #ifdef CONFIG_SND_SOC_SGTL5000 /* To be remved */
 
-static struct regulator_consumer_supply sgtl5000_sabrelite_consumer_vdda = {
+static struct regulator_consumer_supply sgtl5000_hb_consumer_vdda = {
 	.supply = "VDDA",
 	.dev_name = "0-000a",
 };
 
-static struct regulator_consumer_supply sgtl5000_sabrelite_consumer_vddio = {
+static struct regulator_consumer_supply sgtl5000_hb_consumer_vddio = {
 	.supply = "VDDIO",
 	.dev_name = "0-000a",
 };
 
-static struct regulator_consumer_supply sgtl5000_sabrelite_consumer_vddd = {
+static struct regulator_consumer_supply sgtl5000_hb_consumer_vddd = {
 	.supply = "VDDD",
 	.dev_name = "0-000a",
 };
 
-static struct regulator_init_data sgtl5000_sabrelite_vdda_reg_initdata = {
+static struct regulator_init_data sgtl5000_hb_vdda_reg_initdata = {
 	.num_consumer_supplies = 1,
-	.consumer_supplies = &sgtl5000_sabrelite_consumer_vdda,
+	.consumer_supplies = &sgtl5000_hb_consumer_vdda,
 };
 
-static struct regulator_init_data sgtl5000_sabrelite_vddio_reg_initdata = {
+static struct regulator_init_data sgtl5000_hb_vddio_reg_initdata = {
 	.num_consumer_supplies = 1,
-	.consumer_supplies = &sgtl5000_sabrelite_consumer_vddio,
+	.consumer_supplies = &sgtl5000_hb_consumer_vddio,
 };
 
-static struct regulator_init_data sgtl5000_sabrelite_vddd_reg_initdata = {
+static struct regulator_init_data sgtl5000_hb_vddd_reg_initdata = {
 	.num_consumer_supplies = 1,
-	.consumer_supplies = &sgtl5000_sabrelite_consumer_vddd,
+	.consumer_supplies = &sgtl5000_hb_consumer_vddd,
 };
 
-static struct fixed_voltage_config sgtl5000_sabrelite_vdda_reg_config = {
+static struct fixed_voltage_config sgtl5000_hb_vdda_reg_config = {
 	.supply_name		= "VDDA",
 	.microvolts		= 2500000,
 	.gpio			= -1,
-	.init_data		= &sgtl5000_sabrelite_vdda_reg_initdata,
+	.init_data		= &sgtl5000_hb_vdda_reg_initdata,
 };
 
-static struct fixed_voltage_config sgtl5000_sabrelite_vddio_reg_config = {
+static struct fixed_voltage_config sgtl5000_hb_vddio_reg_config = {
 	.supply_name		= "VDDIO",
 	.microvolts		= 3300000,
 	.gpio			= -1,
-	.init_data		= &sgtl5000_sabrelite_vddio_reg_initdata,
+	.init_data		= &sgtl5000_hb_vddio_reg_initdata,
 };
 
-static struct fixed_voltage_config sgtl5000_sabrelite_vddd_reg_config = {
+static struct fixed_voltage_config sgtl5000_hb_vddd_reg_config = {
 	.supply_name		= "VDDD",
 	.microvolts		= 0,
 	.gpio			= -1,
-	.init_data		= &sgtl5000_sabrelite_vddd_reg_initdata,
+	.init_data		= &sgtl5000_hb_vddd_reg_initdata,
 };
 
-static struct platform_device sgtl5000_sabrelite_vdda_reg_devices = {
+static struct platform_device sgtl5000_hb_vdda_reg_devices = {
 	.name	= "reg-fixed-voltage",
 	.id	= 0,
 	.dev	= {
-		.platform_data = &sgtl5000_sabrelite_vdda_reg_config,
+		.platform_data = &sgtl5000_hb_vdda_reg_config,
 	},
 };
 
-static struct platform_device sgtl5000_sabrelite_vddio_reg_devices = {
+static struct platform_device sgtl5000_hb_vddio_reg_devices = {
 	.name	= "reg-fixed-voltage",
 	.id	= 1,
 	.dev	= {
-		.platform_data = &sgtl5000_sabrelite_vddio_reg_config,
+		.platform_data = &sgtl5000_hb_vddio_reg_config,
 	},
 };
 
-static struct platform_device sgtl5000_sabrelite_vddd_reg_devices = {
+static struct platform_device sgtl5000_hb_vddd_reg_devices = {
 	.name	= "reg-fixed-voltage",
 	.id	= 2,
 	.dev	= {
-		.platform_data = &sgtl5000_sabrelite_vddd_reg_config,
+		.platform_data = &sgtl5000_hb_vddd_reg_config,
 	},
 };
 
@@ -452,9 +452,9 @@ static int __init imx6q_init_audio(void)
 			    &mx6_hb_audio_data);
 	imx6q_add_imx_ssi(1, &mx6_hb_ssi_pdata);
 #ifdef CONFIG_SND_SOC_SGTL5000
-	platform_device_register(&sgtl5000_sabrelite_vdda_reg_devices);
-	platform_device_register(&sgtl5000_sabrelite_vddio_reg_devices);
-	platform_device_register(&sgtl5000_sabrelite_vddd_reg_devices);
+	platform_device_register(&sgtl5000_hb_vdda_reg_devices);
+	platform_device_register(&sgtl5000_hb_vddio_reg_devices);
+	platform_device_register(&sgtl5000_hb_vddd_reg_devices);
 #endif
 	return 0;
 }
